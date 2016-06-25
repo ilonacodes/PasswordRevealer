bce3cdb2e16799ae0cdf248f68f39432e5a78c47
Ilona Demidenko <demiluri@gmail.com>
2016-06-26 00:13:14 +0200

    Implement example UI

--- src/PasswordRevealer.js
+++ src/PasswordRevealer.js
@@ -16,6 +16,30 @@ function PasswordRevealer(passwordController, revealController) {
     }
 }
 
+function PasswordController() {
+    var password = document.getElementById("password")
+
+    this.reveal = function () {
+        password.type = "text"
+    }
+
+    this.hide = function () {
+        password.type = "password"
+    }
+}
+
+function RevealController() {
+    var toggleButton = document.getElementById("password_revealer_toggle_button")
+
+    this.turnOn = function () {
+        toggleButton.value = "Hide"
+    }
+
+    this.turnOff = function () {
+        toggleButton.value = "Reveal"
+    }
+}
+
 if (typeof module != "undefined") {
     module.exports = PasswordRevealer
 }
